8dbb5e33cbbfeec441b255cf435cfa56a3d52538
GUNN33RR <ghanemismail16@gmail.com>
2024-01-16 13:34:33 -0700

    in class daily

---
+++ javascriptbasics.js
@@ -0,0 +1,45 @@
+// Create a person/user object 
+const person = {
+  firstName: "Ghanem",
+  lastName: "Ismail",
+};
+
+// Create an array of numbers
+const numbersArray = [5, 10, 15, 20];
+
+// Function to compare two numbers and return the larger number
+function compareNumbers(num1, num2) {
+  // Check if num1 or num2 are not numbers, return an error if true
+  if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+    return "Error: Both values must be numbers.";
+  }
+
+  // Compare the two numbers and return the larger one
+  if (num1 > num2) {
+    return num1;
+  } else if (num2 > num1) {
+    return num2;
+  } else {
+    return "Both numbers are equal.";
+  }
+}
+
+// Function to return the user's full name 
+function getFullName(user) {
+  // Check if the user object has firstName and lastName properties
+  if (user.firstName && user.lastName) {
+    return `${user.firstName} ${user.lastName}`;
+  } else {
+    return "Invalid user object, missing firstName or lastName.";
+  }
+}
+
+// Example usage of the functions
+const num1 = 17;
+const num2 = 17;
+
+const largerNumber = compareNumbers(num1, num2);
+console.log("Larger Number:", largerNumber);
+
+const fullName = getFullName(person);
+console.log("Full Name:", fullName);
---
+++ readme.md
@@ -0,0 +1,2 @@
+<a href="https://gunn33rr.github.io/workspace1/"> Ghanem Ismail Site Link</a>
+<a href="https://github.com/GUNN33RR/workspace1"> Ghanem Ismail Github Site Link</a>
